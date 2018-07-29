25bb6f796d0967c03319b949d6a8bef5cb41bf59
小西壮 <>
2018-07-29 20:37:24 +0800

    問題数、得点計算など追加、

--- answer.php
+++ answer.php
@@ -1,14 +1,59 @@
 <?php
 
-$question = $_POST['question'];
-$answer = $_POST['answer'];
+$question = array(
+  'a' => $_POST['question_a'],
+  'b' => $_POST['question_b'],
+  'c' => $_POST['question_c'],
+  'd' => $_POST['question_d'],
+  'e' => $_POST['question_e'],
+);
 
-if($question == $answer){
-  $result = "正解！";
+$answer = array(
+  'a' => $_POST['answer_a'],
+  'b' => $_POST['answer_b'],
+  'c' => $_POST['answer_c'],
+  'd' => $_POST['answer_d'],
+  'e' => $_POST['answer_e'],
+);
+
+$result = array();
+$score = 0;
+
+if($question['a'] == $answer['a']){
+  $result['a'] = "正解！";
+  $score += 20;
 }else{
-  $result = "不正解･･･";
+  $result['a'] = "不正解･･･";
 }
- 
+
+if($question['b'] == $answer['b']){
+  $result['b'] = "正解！";
+  $score += 20;
+}else{
+  $result['b'] = "不正解･･･";
+}
+
+if($question['c'] == $answer['c']){
+  $result['c'] = "正解！";
+  $score += 20;
+}else{
+  $result['c'] = "不正解･･･";
+}
+
+if($question['d'] == $answer['d']){
+  $result['d'] = "正解！";
+  $score += 20;
+}else{
+  $result['d'] = "不正解･･･";
+}
+
+if($question['e'] == $answer['e']){
+  $result['e'] = "正解！";
+  $score += 20;
+}else{
+  $result['e'] = "不正解･･･";
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -28,8 +73,34 @@ if($question == $answer){
     </header>
 
     <div id="container">
-      <h2>クイズの結果</h2>
-      <h2><?php echo $result ?></h2>
+      <h2>クイズの結果!</h2>
+      <h2><?php echo $score ?>点</h2>
+
+      <div class="section">
+        <p>Q1.<?php echo $result['a'] ?></p>
+        <p>答え、　<?php echo $answer['a'] ?></p>
+        <p>あなたの答え、<?php echo $question['a'] ?></p>
+      </div>
+      <div class="section">
+        <p>Q2.<?php echo $result['b'] ?></p>
+        <p>答え、　<?php echo $answer['b'] ?></p>
+        <p>あなたの答え、<?php echo $question['b'] ?></p>
+      </div>
+      <div class="section">
+        <p>Q3.<?php echo $result['c'] ?></p>
+        <p>答え、　<?php echo $answer['c'] ?></p>
+        <p>あなたの答え、<?php echo $question['c'] ?></p>
+      </div>
+      <div class="section">
+        <p>Q4.<?php echo $result['d'] ?></p>
+        <p>答え、　<?php echo $answer['d'] ?></p>
+        <p>あなたの答え、<?php echo $question['d'] ?></p>
+      </div>
+      <div class="section">
+        <p>Q5.<?php echo $result['e'] ?></p>
+        <p>答え、　<?php echo $answer['e'] ?></p>
+        <p>あなたの答え、<?php echo $question['e'] ?></p>
+      </div>
     </div>
 
     <footer id="footer">
--- quiz.php
+++ quiz.php
@@ -1,13 +1,39 @@
 <?php
  
-$title = 'ハイジャックの語源とは？';
- 
-$choices = array();
-$choices = array('ジャックという名前が多かったから','空高いところで襲撃するから','テンションがハイだから','高い金銭を要求するから');
- 
-$answer = $choices[0];
- 
-shuffle($choices);
+$title_a = 'Q1,ハイジャックの語源とは？';
+$title_b = 'Q2,点滴の成分は何と同じ？';
+$title_c = 'Q3,瞬間接着剤は何のために開発された？';
+$title_d = 'Q4,アブラカタブラの意味とは？';
+$title_e = 'Q5,日本で初めてのペットボトルは、何の容器だったでしょうか？';
+
+
+$choices_a = array();
+$choices_a = array('ジャックという名前が多かったから','空高いところで襲撃するから','テンションがハイだから','高い金銭を要求するから');
+
+$choices_b = array();
+$choices_b = array('ポカリスエット','血液','オレンジジュース','砂糖水');
+
+$choices_c = array();
+$choices_c = array('戦争','漁業','農業','工作');
+
+$choices_d = array();
+$choices_d = array('花粉症対策','いただきます','悪霊退散','ごちそうさまでした');
+
+$choices_e = array();
+$choices_e = array('醤油','コーラ','コーヒー','ミネラルウォーター');
+
+$answer_a = $choices_a[0];
+$answer_b = $choices_b[0];
+$answer_c = $choices_c[0];
+$answer_d = $choices_d[0];
+$answer_e = $choices_e[0];
+
+shuffle($choices_a);
+shuffle($choices_b);
+shuffle($choices_c);
+shuffle($choices_d);
+shuffle($choices_e);
+
  
 ?>
 
@@ -29,15 +55,49 @@ shuffle($choices);
 
     <div id="container">
       <h2>雑学クイズ！</h2>
-      <h1>ハイジャックの語源とは？</h1>
 
       <form method="POST" action="answer.php">
+        <h1><?php echo $title_a ?></h1>
         <ul>
-          <?php foreach($choices as $choice){ ?>
-            <li><input type="radio" name="question" value="<?php echo $choice; ?>">  <?php echo $choice; ?></li>
+          <?php foreach($choices_a as $choice_a){ ?>
+            <li><input type="radio" name="question_a" value="<?php echo $choice_a; ?>">  <?php echo $choice_a; ?></li>
           <?php } ?>
         </ul><br>
-        <input type="hidden" name="answer" value="<?php echo $answer ?>">
+
+        <h1><?php echo $title_b ?></h1>
+        <ul>
+          <?php foreach($choices_b as $choice_b){ ?>
+            <li><input type="radio" name="question_b" value="<?php echo $choice_b; ?>">  <?php echo $choice_b; ?></li>
+          <?php } ?>
+        </ul><br>
+
+        <h1><?php echo $title_c ?></h1>
+        <ul>
+          <?php foreach($choices_c as $choice_c){ ?>
+            <li><input type="radio" name="question_c" value="<?php echo $choice_c; ?>">  <?php echo $choice_c; ?></li>
+          <?php } ?>
+        </ul><br>
+
+        <h1><?php echo $title_d ?></h1>
+        <ul>
+          <?php foreach($choices_d as $choice_d){ ?>
+            <li><input type="radio" name="question_d" value="<?php echo $choice_d; ?>">  <?php echo $choice_d; ?></li>
+          <?php } ?>
+        </ul><br>
+
+        <h1><?php echo $title_e ?></h1>
+        <ul>
+          <?php foreach($choices_e as $choice_e){ ?>
+            <li><input type="radio" name="question_e" value="<?php echo $choice_e; ?>">  <?php echo $choice_e; ?></li>
+          <?php } ?>
+        </ul><br>
+
+        <input type="hidden" name="answer_a" value="<?php echo $answer_a ?>">
+        <input type="hidden" name="answer_b" value="<?php echo $answer_b ?>">
+        <input type="hidden" name="answer_c" value="<?php echo $answer_c ?>">
+        <input type="hidden" name="answer_d" value="<?php echo $answer_d ?>">
+        <input type="hidden" name="answer_e" value="<?php echo $answer_e ?>">
+
         <input type="submit" class="btn btn-primary" value="回答する!">
       </form>
     </div>
