852012934d1dc6c6856a72ce4aff12e00b7f62c8
Atsushi <>
2018-07-30 23:38:25 +0800

    first commit

--- answer.php
+++ answer.php
@@ -1,57 +1,21 @@
 <?php
 
 $question = array(
-  'a' => $_POST['question_a'],
-  'b' => $_POST['question_b'],
-  'c' => $_POST['question_c'],
-  'd' => $_POST['question_d'],
-  'e' => $_POST['question_e'],
+    'a' => $_POST['question_a'],
 );
 
 $answer = array(
-  'a' => $_POST['answer_a'],
-  'b' => $_POST['answer_b'],
-  'c' => $_POST['answer_c'],
-  'd' => $_POST['answer_d'],
-  'e' => $_POST['answer_e'],
+    'a' => $_POST['answer_a'],
 );
 
 $result = array();
 $score = 0;
 
-if($question['a'] == $answer['a']){
-  $result['a'] = "正解！";
-  $score += 20;
-}else{
-  $result['a'] = "不正解･･･";
-}
-
-if($question['b'] == $answer['b']){
-  $result['b'] = "正解！";
-  $score += 20;
-}else{
-  $result['b'] = "不正解･･･";
-}
-
-if($question['c'] == $answer['c']){
-  $result['c'] = "正解！";
-  $score += 20;
-}else{
-  $result['c'] = "不正解･･･";
-}
-
-if($question['d'] == $answer['d']){
-  $result['d'] = "正解！";
-  $score += 20;
-}else{
-  $result['d'] = "不正解･･･";
-}
-
-if($question['e'] == $answer['e']){
-  $result['e'] = "正解！";
-  $score += 20;
-}else{
-  $result['e'] = "不正解･･･";
+if ($question['a'] == $answer['a']) {
+    $result['a'] = "正解！";
+    $score += 20;
+} else {
+    $result['a'] = "不正解･･･";
 }
 
 ?>
@@ -80,26 +44,6 @@ if($question['e'] == $answer['e']){
         <p>Q1.<?php echo $result['a'] ?></p>
         <p>答え、　<?php echo $answer['a'] ?></p>
         <p>あなたの答え、<?php echo $question['a'] ?></p>
-      </div>
-      <div class="section">
-        <p>Q2.<?php echo $result['b'] ?></p>
-        <p>答え、　<?php echo $answer['b'] ?></p>
-        <p>あなたの答え、<?php echo $question['b'] ?></p>
-      </div>
-      <div class="section">
-        <p>Q3.<?php echo $result['c'] ?></p>
-        <p>答え、　<?php echo $answer['c'] ?></p>
-        <p>あなたの答え、<?php echo $question['c'] ?></p>
-      </div>
-      <div class="section">
-        <p>Q4.<?php echo $result['d'] ?></p>
-        <p>答え、　<?php echo $answer['d'] ?></p>
-        <p>あなたの答え、<?php echo $question['d'] ?></p>
-      </div>
-      <div class="section">
-        <p>Q5.<?php echo $result['e'] ?></p>
-        <p>答え、　<?php echo $answer['e'] ?></p>
-        <p>あなたの答え、<?php echo $question['e'] ?></p>
       </div>
     </div>
 
